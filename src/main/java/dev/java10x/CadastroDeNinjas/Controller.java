9580ae7d8244fc13a0afbfbce7cec3d88caa4984
theycallmefreire <guilherme120@gmail.com>
2025-09-18 01:40:14 -0300

    adicionado primeira rota

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,17 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasVindas") //pegar informações
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+
+}
