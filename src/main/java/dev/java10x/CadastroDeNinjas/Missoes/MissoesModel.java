87a6d7dc4b2c17358ee2e0f49e720f4681e1b045
theycallmefreire <guilherme120@gmail.com>
2025-09-18 16:26:32 -0300

    Adicionando LomboÃk

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.Controler.Service.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -18,7 +24,5 @@ public class MissoesModel {
     // OneToMany uma missão pode ter vários ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
-
-
 
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controler/Service/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controler/Service/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas.Controler.Service;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,6 +12,9 @@ import java.util.List;
 // JPA = Java Persistence API
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -23,37 +29,5 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")// foreing key ou chave estrangeira
     private MissoesModel missoes;
 
-    public NinjaModel(){
 
-    }
-
-    public NinjaModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
